dc54bd38883260533aa07a259cf6f54bfede0d1a
Inn0cent_Zer0 <157284454+Inn0cent-Zer0@users.noreply.github.com>
2025-07-04 20:32:54 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,167 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which HTML element do we put the JavaScript?",
+    answer: "<script>",
+    options: [
+      "<javascript>",
+      "<js>",
+      "<script>",
+      "<code>"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which property is used to change the background color in CSS?",
+    answer: "background-color",
+    options: [
+      "color",
+      "background-color",
+      "bgColor",
+      "background-image"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which symbol is used for comments in JavaScript?",
+    answer: "//",
+    options: [
+      "//",
+      "<!-- -->",
+      "#",
+      "**"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which HTML tag is used to link JavaScript files?",
+    answer: "<script>",
+    options: [
+      "<javascript>",
+      "<script>",
+      "<link>",
+      "<js>"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which company developed the React library?",
+    answer: "Facebook",
+    options: [
+      "Google",
+      "Facebook",
+      "Microsoft",
+      "Twitter"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Inside which HTML element do we write CSS?",
+    answer: "<style>",
+    options: [
+      "<css>",
+      "<script>",
+      "<style>",
+      "<link>"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does JSON stand for?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "JavaScript Oriented Notation",
+      "Java Syntax Object Notation",
+      "JavaScript Object Notation",
+      "Java Source Open Network"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is NOT a JavaScript framework?",
+    answer: "Django",
+    options: [
+      "Angular",
+      "Vue",
+      "Django",
+      "React"
+    ]
+  },
+  {
+    numb: 11,
+    question: "In CSS, what does the 'float' property do?",
+    answer: "It positions elements to the left or right",
+    options: [
+      "It adds background images",
+      "It sets font size",
+      "It positions elements to the left or right",
+      "It changes text color"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which method converts JSON data to a JavaScript object?",
+    answer: "JSON.parse()",
+    options: [
+      "JSON.parse()",
+      "JSON.stringify()",
+      "JSON.object()",
+      "JSON.convert()"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which git command is used to upload local commits to a remote repository?",
+    answer: "git push",
+    options: [
+      "git pull",
+      "git push",
+      "git commit",
+      "git clone"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which HTTP status code means 'Not Found'?",
+    answer: "404",
+    options: [
+      "200",
+      "301",
+      "404",
+      "500"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Strong Question Language",
+      "Structured Query Language",
+      "Sequential Query Language",
+      "Stylish Question Language"
+    ]
+  }
+];
